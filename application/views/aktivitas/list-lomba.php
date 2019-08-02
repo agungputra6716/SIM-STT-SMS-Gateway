fb3418488fbbbeb2f9455f2f1bb4d2f5b0870ff0
Turah Putra <agungputra6716@gmail.com>
2019-08-03 00:57:22 +0800

    Refining assets

--- application/views/aktivitas/list-lomba.php
+++ application/views/aktivitas/list-lomba.php
@@ -10,23 +10,11 @@
   <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url('bower_components/font-awesome/css/font-awesome.min.css') ?>">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="<?php echo base_url('bower_components/Ionicons/css/ionicons.min.css') ?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url('dist/css/AdminLTE.min.css') ?>">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="<?php echo base_url('dist/css/skins/_all-skins.min.css') ?>">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="<?php echo base_url('bower_components/morris.js/morris.css') ?>">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="<?php echo base_url('bower_components/jvectormap/jquery-jvectormap.css') ?>">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') ?>">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?php echo base_url('bower_components/bootstrap-daterangepicker/daterangepicker.css') ?>">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="<?php echo base_url('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') ?>">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -297,43 +285,17 @@
 
 <!-- jQuery 3 -->
 <script src="<?php echo base_url('bower_components/jquery/dist/jquery.min.js') ?>"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="<?php echo base_url('bower_components/jquery-ui/jquery-ui.min.js') ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url('bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
-<!-- Morris.js charts -->
-<script src="<?php echo base_url('bower_components/raphael/raphael.min.js') ?>"></script>
-<script src="<?php echo base_url('bower_components/morris.js/morris.min.js') ?>"></script>
-<!-- Sparkline -->
-<script src="<?php echo base_url('bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') ?>"></script>
-<!-- jvectormap -->
-<script src="<?php echo base_url('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>"></script>
-<script src="<?php echo base_url('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>"></script>
-<!-- jQuery Knob Chart -->
-<script src="<?php echo base_url('bower_components/jquery-knob/dist/jquery.knob.min.js') ?>"></script>
-<!-- daterangepicker -->
-<script src="<?php echo base_url('bower_components/moment/min/moment.min.js') ?>"></script>
-<script src="<?php echo base_url('bower_components/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
-<!-- datepicker -->
-<script src="<?php echo base_url('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') ?>"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="<?php echo base_url('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>"></script>
-<!-- Slimscroll -->
-<script src="<?php echo base_url('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url('bower_components/fastclick/lib/fastclick.js') ?>"></script>
-<!-- AdminLTE App -->
 <script src="<?php echo base_url('dist/js/adminlte.min.js') ?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url('dist/js/pages/dashboard.js') ?>"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url('dist/js/demo.js') ?>"></script>
 <script src="<?php echo base_url('bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
 <script src="<?php echo base_url('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
 
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
+<script>  
   $(document).ready(function() {
     $('#btn_kelola_lomba').addClass('active');
     $('#tb_list_kegiatan').DataTable();
